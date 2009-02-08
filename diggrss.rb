f3d4d60ea582362ea2c8c6c038443bf685d35a99
Nate Todd <nate@natetodd.com>
2009-02-07 23:05:20 -0500

    vendored all dependencies.  most of the app is done, but chronic won't parse out the relative time properly, so it's just set to the current time.  bad form

--- diggrss.rb
+++ diggrss.rb
@@ -2,29 +2,42 @@ $:.unshift *Dir[File.dirname(__FILE__) + '/vendor/*/lib']
 require 'rubygems'
 require 'sinatra'
 require 'builder'
+require 'hpricot'
+require 'open-uri'
+require 'chronic'
 
 get '/' do
   "Instructions"
 end
 
-get '/:channel' do
+get '/:channel' do  
+  doc = open("http://digg.com/#{params[:channel]}/", 'User-Agent' => 'screw-you-digg') { |f| Hpricot(f) }
+  # stories = doc.search('//div.news-body')
+  
+  # puts stories
+  
   builder do |xml|
     xml.instruct! :xml, :version => '1.0'
     xml.rss :version => "2.0" do
       xml.channel do
-        xml.title "Digg - #{params[:channel].capitalize}"
+        xml.title "DiggRSS - #{params[:channel].capitalize}"
         xml.description "DiggRSS feed."
         xml.link "http://digg.com/#{params[:channel]}"
         
-        # @posts.each do |post|
+        doc.search('//div.news-body').each do |story|
+          
+          # pubtime = story.search('//div.news-details/span/span/span/').to_s
+          pubtime = Time.now
+          
           xml.item do
-            xml.title "Item Title"
-            xml.link "http://liftoff.msfc.nasa.gov/posts/1"
-            xml.description "Item Description"
+            xml.title story.search('//h3/a/')
+            xml.link story.search('//h3/a').first[:href]
+            xml.description story.search('//p/')
+            # xml.pubDate Chronic.parse(story.search('//div.news-details/span/span/span/').to_s)
             xml.pubDate Time.now.rfc822()
-            xml.guid "http://liftoff.msfc.nasa.gov/posts/1"
+            xml.guid story.search('//h3/a').first[:href]
           end
-        # end
+        end
       end
     end
   end
