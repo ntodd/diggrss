fdf25b57a88dff9998de6311ff40dc6daad67abf
Nate Todd <nate@natetodd.com>
2009-02-07 21:20:14 -0500

    Initial import

---
+++ diggrss.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'sinatra'
+require 'builder'
+
+get '/' do
+  "Instructions"
+end
+
+get '/:channel' do
+  builder do |xml|
+    xml.instruct! :xml, :version => '1.0'
+    xml.rss :version => "2.0" do
+      xml.channel do
+        xml.title "Digg - #{params[:channel].capitalize}"
+        xml.description "DiggRSS feed."
+        xml.link "http://digg.com/#{params[:channel]}"
+        
+        # @posts.each do |post|
+          xml.item do
+            xml.title "Item Title"
+            xml.link "http://liftoff.msfc.nasa.gov/posts/1"
+            xml.description "Item Description"
+            xml.pubDate Time.now.rfc822()
+            xml.guid "http://liftoff.msfc.nasa.gov/posts/1"
+          end
+        # end
+      end
+    end
+  end
+end
